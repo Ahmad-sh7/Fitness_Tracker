0cdc8ff2662f7e204b7954cac1b45ecef47586ba
Katharina Lara Neuhaus <katharina.neuhaus@ruhr-uni-bochum.de>
2022-05-04 15:31:58 +0200

    Made Activity Page

---
+++ app/src/main/java/com/example/myfitnesstracker/ActivitiesPageActivity.java
@@ -0,0 +1,14 @@
+package com.example.myfitnesstracker;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ActivitiesPageActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_activities_page);
+    }
+}
