0f265067dda8319b5753d8db8d7f28dc08452469
bahaaabiad <95656571+bahaaabiad@users.noreply.github.com>
2022-05-15 14:48:18 +0200

    Create Mainactivity2.java

---
+++ app/src/main/java/com/example/myfitnesstracker/Mainactivity2.java
@@ -0,0 +1,41 @@
+package com.example.moodrecorder;
+
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity2 extends AppCompatActivity {
+    private Button button;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main2);
+        button = (Button) findViewById(R.id.zurück2);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openActivity1();
+            }
+        });
+        button = (Button) findViewById(R.id.weiter2);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openActivity3();
+            }
+        });
+    }
+    public void openActivity1()
+    {
+        Intent intent = new Intent(this, MainActivity0.class);
+        startActivity(intent);
+    }
+
+public void openActivity3()
+        {
+        Intent intent = new Intent(this, MainActivity3.class);
+        startActivity(intent); }
+        
