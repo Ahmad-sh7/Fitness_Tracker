35a7bfa5e62e00378217d1e1f139224638948cfc
bahaaabiad <95656571+bahaaabiad@users.noreply.github.com>
2022-05-15 14:47:02 +0200

    Update MainActivity.java

--- app/src/main/java/com/example/myfitnesstracker/MainActivity.java
+++ app/src/main/java/com/example/myfitnesstracker/MainActivity.java
@@ -1,9 +1,8 @@
 package com.example.myfitnesstracker;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
@@ -16,10 +15,11 @@ public class MainActivity extends AppCompatActivity {
 
     /** Called when the user taps the Log Activity button */
     public void goToActivityPage(View view) {
-        Intent intent = new Intent(this, ActivitiesPageActivity.class);
+        Intent intent = new Intent(this, MainActivity0.class);
         startActivity(intent);
 
     }
-
-
 }
+
+
+
