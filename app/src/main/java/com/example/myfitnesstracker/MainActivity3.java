98a1b33592607d8f93db1295ad692b648663b878
bahaaabiad <95656571+bahaaabiad@users.noreply.github.com>
2022-05-18 20:35:45 +0200

    Update MainActivity3.java

--- app/src/main/java/com/example/myfitnesstracker/MainActivity3.java
+++ app/src/main/java/com/example/myfitnesstracker/MainActivity3.java
@@ -6,14 +6,35 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.SeekBar;
+import android.widget.TextView;
 
 public class MainActivity3 extends AppCompatActivity {
     private Button button;
+    private SeekBar seekBar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main3);
+        SeekBar seekBar = findViewById(R.id.seekBarID9);
+        TextView textView = findViewById(R.id.progress9);
+        seekBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+                textView.setText(String.valueOf(progress) + "%");
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+
+            }
+        });
         button = (Button) findViewById(R.id.zurück3);
         button.setOnClickListener(new View.OnClickListener() {
             @Override
