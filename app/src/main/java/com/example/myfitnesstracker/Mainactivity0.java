dabef08812db431e00a13f5668954597a11a43f6
bahaaabiad <95656571+bahaaabiad@users.noreply.github.com>
2022-05-18 20:18:06 +0200

    Update Mainactivity0.java
    SeekBar shows progress now

--- app/src/main/java/com/example/myfitnesstracker/Mainactivity0.java
+++ app/src/main/java/com/example/myfitnesstracker/Mainactivity0.java
@@ -6,14 +6,133 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.SeekBar;
+import android.widget.TextView;
 
 public class MainActivity0 extends AppCompatActivity {
     private Button button;
+    private SeekBar seekBar1;
+    private SeekBar seekBar2;
+    private SeekBar seekBar3;
+    private SeekBar seekBar4;
+    private SeekBar seekBar5;
+    private SeekBar seekBar6;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main0);
+        SeekBar seekBar1 = findViewById(R.id.seekBarID1);
+        TextView textView1 = findViewById(R.id.progress1);
+        SeekBar seekBar2 = findViewById(R.id.seekBarID2);
+        TextView textView2 = findViewById(R.id.progress2);
+        SeekBar seekBar3 = findViewById(R.id.seekBarID3);
+        TextView textView3 = findViewById(R.id.progress3);
+        SeekBar seekBar4 = findViewById(R.id.seekBarID4);
+        TextView textView4 = findViewById(R.id.progress4);
+        SeekBar seekBar5 = findViewById(R.id.seekBarID5);
+        TextView textView5 = findViewById(R.id.progress5);
+        SeekBar seekBar6 = findViewById(R.id.seekBarID6);
+        TextView textView6 = findViewById(R.id.progress6);
+
+        seekBar1.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar1, int progress1, boolean fromUser) {
+                textView1.setText(String.valueOf(progress1) + "%");
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+
+            }
+        });
+        seekBar2.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar2, int progress2, boolean fromUser) {
+                textView2.setText(String.valueOf(progress2) + "%");
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+
+            }
+        });
+        seekBar3.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar3, int progress3, boolean fromUser) {
+                textView3.setText(String.valueOf(progress3) + "%");
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+
+            }
+        });
+        seekBar4.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar4, int progress4, boolean fromUser) {
+                textView4.setText(String.valueOf(progress4) + "%");
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+
+            }
+        });
+        seekBar5.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar5, int progress5, boolean fromUser) {
+                textView5.setText(String.valueOf(progress5) + "%");
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+
+            }
+        });
+        seekBar6.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar6, int progress6, boolean fromUser) {
+                textView6.setText(String.valueOf(progress6) + "%");
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+
+            }
+        });
+
         button = (Button) findViewById(R.id.weiter1);
         button.setOnClickListener(new View.OnClickListener() {
             @Override
