aa5bb24e443a130046383667489965f9d2fadc94
bahaaabiad <95656571+bahaaabiad@users.noreply.github.com>
2022-05-18 20:51:14 +0200

    Update MainActivity.java
    New Button for the user to start recording his mood anytime he wants.

--- app/src/main/java/com/example/myfitnesstracker/MainActivity.java
+++ app/src/main/java/com/example/myfitnesstracker/MainActivity.java
@@ -6,20 +6,38 @@ import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
+    private Button button;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    button = (Button) findViewById(R.id.button2);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openActivity0();
+            }
+        });
     }
 
-    /** Called when the user taps the Log Activity button */
-    public void goToActivityPage(View view) {
+    public void openActivity0() {
         Intent intent = new Intent(this, MainActivity0.class);
         startActivity(intent);
-
     }
+
+    /** Called when the user taps the Log Activity button
+     *
+        button = (Button) findViewById(R.id.button);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+             public void onClick(View v) {
+                 goToActivityPage();
+             }
+         });
+     public void goToActivityPage(View view) {
+        Intent intent = new Intent(this, Activity.class);
+        startActivity(intent);
+     }
+     */
 }
-
-
-
