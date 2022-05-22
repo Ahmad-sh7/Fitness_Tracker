9b4d3a8e1477132ae390ff35ed4cfe2335ac5230
Ahmad-sh7 <82279080+Ahmad-sh7@users.noreply.github.com>
2022-05-22 17:36:33 +0200

    Add files via upload

---
+++ app/src/main/java/com/example/myfitnesstracker/model/AppDatabase.java
@@ -0,0 +1,15 @@
+package com.example.myfitnesstracker.model;
+
+
+import androidx.room.Database;
+import androidx.room.RoomDatabase;
+import androidx.room.TypeConverter;
+import androidx.room.TypeConverters;
+
+import com.example.myfitnesstracker.model.typeconverter.SensorDataListTypeConverter;
+
+@Database(entities = {ActivtyRecord.class},version = 1)
+@TypeConverters(SensorDataListTypeConverter.class)
+public abstract class AppDatabase extends RoomDatabase {
+    public abstract SensorDataDao sensorDataDao();
+}
