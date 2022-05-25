a442d16ecf492bca1ca036324bf439fc05fa2588
Katharina Lara Neuhaus <katharina.neuhaus@ruhr-uni-bochum.de>
2022-05-25 12:02:18 +0200

    started on mood chart

--- app/src/main/java/com/example/myfitnesstracker/StatisticsPageActivity.java
+++ app/src/main/java/com/example/myfitnesstracker/StatisticsPageActivity.java
@@ -10,10 +10,12 @@ import com.github.mikephil.charting.data.BarData;
 import com.github.mikephil.charting.data.BarDataSet;
 import com.github.mikephil.charting.data.BarEntry;
 import com.github.mikephil.charting.charts.LineChart;
+import com.github.mikephil.charting.data.Entry;
 import com.github.mikephil.charting.data.LineData;
 import com.github.mikephil.charting.data.LineDataSet;
 //import com.github.mikephil.charting.data.LineEntry;
 import com.github.mikephil.charting.utils.ColorTemplate;
+import java.util.*;
 import java.util.ArrayList;
 
 public class StatisticsPageActivity extends AppCompatActivity {
@@ -32,41 +34,47 @@ public class StatisticsPageActivity extends AppCompatActivity {
         lineChartMood = findViewById(R.id.line_chart_mood);
 
         makeBarChart();
+        makeLineChart();
 
     }
 
 
     private void makeBarChart(){
 
-        // Initialize Array List
-        ArrayList<BarEntry> barActivityEntries = new ArrayList<>();
+        ArrayList<BarEntry> barActivityEntries = new ArrayList<>();// Initialize Array List
 
         // Example Instances
         for (int i=1; i<=7; i++){
-
-            // Convert To Float
-            float value = (float) ((i+1)*10.0);
-
-            // Initialize Bar and Pie Chart Entry
-            BarEntry barEntry = new BarEntry(i, value);
-
-            // Add Values in Array List
-            barActivityEntries.add(barEntry);
+            float value = (float) ((i+1)*10.0);// Convert To Float
+            BarEntry barEntry = new BarEntry(i, value);// Initialize Bar and Pie Chart Entry
+            barActivityEntries.add(barEntry);// Add Values in Array List
         }
 
-        // Initialize Bar Data Set
-        BarDataSet barDataSet = new BarDataSet(barActivityEntries, "Daily Activity in minutes");
-        barDataSet.setColors(Color.rgb(55,0,179));
-
-        // Set Bar Data
-        barChartActivity.setData(new BarData(barDataSet));
-
-        // Set Animations
-        barChartActivity.animateY(5000);
-
-        // Removing Description text
-        barChartActivity.getDescription().setText(" ");
+        BarDataSet barDataSet = new BarDataSet(barActivityEntries, "Daily Activity in minutes");// Initialize Bar Data Set
+        barDataSet.setColors(Color.rgb(55,0,179));// Set Bar Color
+        barChartActivity.setData(new BarData(barDataSet));// Set Bar Data
+        barChartActivity.animateY(5000);// Set Animations
+        barChartActivity.getDescription().setText(" ");// Removing Description text
         //barChartActivity.getDescription().setTextColor(Color.WHITE);
+    }
+
+    private void makeLineChart(){
+
+        ArrayList<ArrayList<Entry>> linesMoodEntries = new ArrayList<ArrayList<Entry>>(); // List of all Moods
+        ArrayList<Entry> Mood1 = new ArrayList<>();
+        ArrayList<Entry> Mood2 = new ArrayList<>();
+        ArrayList<Entry> Mood3 = new ArrayList<>();
+        ArrayList<Entry> Mood4 = new ArrayList<>();
+        ArrayList<Entry> Mood5 = new ArrayList<>();
+        ArrayList<Entry> Mood6 = new ArrayList<>();
+
+        linesMoodEntries.add(Mood1);
+        linesMoodEntries.add(Mood2);
+        linesMoodEntries.add(Mood3);
+        linesMoodEntries.add(Mood4);
+        linesMoodEntries.add(Mood5);
+        linesMoodEntries.add(Mood6);
+
     }
 
 }
