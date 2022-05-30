3876012c401335aa6ec3e67af6cccd7e2b5914fe
Ahmad-sh7 <82279080+Ahmad-sh7@users.noreply.github.com>
2022-05-30 19:23:44 +0200

    Update ActivitiesPageActivity.java

--- app/src/main/java/com/example/myfitnesstracker/ActivitiesPageActivity.java
+++ app/src/main/java/com/example/myfitnesstracker/ActivitiesPageActivity.java
@@ -1,34 +1,31 @@
-package com.example.myfitnesstracker;
-
-import androidx.appcompat.app.AppCompatActivity;
+package com.example.myfitnesstracker.view.activities;
 
 import android.content.Context;
-import android.content.Intent;
+import android.content.DialogInterface;
 import android.hardware.Sensor;
 import android.hardware.SensorEvent;
 import android.hardware.SensorEventListener;
 import android.hardware.SensorManager;
 import android.os.AsyncTask;
 import android.os.Bundle;
-import android.util.Log;
-import android.view.View.OnClickListener;
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.room.Room;
-
-import android.os.Bundle;
+import android.os.CountDownTimer;
 import android.os.Handler;
 import android.view.View;
-import android.widget.AdapterView;
+import android.view.View.OnClickListener;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.Spinner;
 import android.widget.TextView;
-import android.widget.Toast;
 
+import androidx.appcompat.app.AlertDialog;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.room.Room;
+
+import com.example.myfitnesstracker.R;
 import com.example.myfitnesstracker.model.ActivtyRecord;
 import com.example.myfitnesstracker.model.AppDatabase;
 import com.example.myfitnesstracker.model.SensorData;
-import com.example.myfitnesstracker.model.SensorDataDao;
+import com.example.myfitnesstracker.model.dao.SensorDataDao;
 
 import java.util.ArrayList;
 import java.util.Timer;
@@ -93,9 +90,9 @@ public class ActivitiesPageActivity extends AppCompatActivity implements SensorE
 
         handler = new Handler();
 
-        txt_x = findViewById(R.id.textView2); //create x axis object
-        txt_y = findViewById(R.id.textView3); // create y axis object
-        txt_z = findViewById(R.id.textView4); //create z axis object
+        //txt_x = findViewById(R.id.textView2); //create x axis object
+        //txt_y = findViewById(R.id.textView3); // create y axis object
+        //txt_z = findViewById(R.id.textView4); //create z axis object
 
 
 
@@ -137,9 +134,9 @@ public class ActivitiesPageActivity extends AppCompatActivity implements SensorE
             double changedAcceleration = Math.abs(accelerationCurrentValue - accelerationPreviousValue);
             accelerationPreviousValue = accelerationCurrentValue;
 
-            txt_x.setText(String.format("Current =%s", accelerationCurrentValue));
-            txt_y.setText(String.format("prev =%s", accelerationPreviousValue));
-            txt_z.setText(String.format("changed =%s", changedAcceleration));
+            //txt_x.setText(String.format("Current =%s", accelerationCurrentValue));
+            //txt_y.setText(String.format("prev =%s", accelerationPreviousValue));
+            //txt_z.setText(String.format("changed =%s", changedAcceleration));
 
             if (isFirstTime){
                 int begin=0;
@@ -152,7 +149,6 @@ public class ActivitiesPageActivity extends AppCompatActivity implements SensorE
                                 System.currentTimeMillis(),
                                 x,y,z
                         ));
-          
 
                     }
                 },begin,2000);
@@ -163,9 +159,9 @@ public class ActivitiesPageActivity extends AppCompatActivity implements SensorE
 
 
         }else{
-            txt_x.setText("Waiting for sensor data..");
-            txt_y.setText("Waiting for sensor data..");
-            txt_z.setText("Waiting for sensor data..");
+            //txt_x.setText("Waiting for sensor data..");
+            //txt_y.setText("Waiting for sensor data..");
+            //txt_z.setText("Waiting for sensor data..");
 
         }
     }
@@ -187,7 +183,7 @@ public class ActivitiesPageActivity extends AppCompatActivity implements SensorE
                 stopButton.setEnabled(true);
                 startButton.setEnabled(false);
                 flag=true;
-                dialogTimer.start(); //start timer as soon as sport is started
+                dialogTimer.start();
 
                 timer2.schedule(new TimerTask() {
                     @Override
@@ -218,8 +214,7 @@ public class ActivitiesPageActivity extends AppCompatActivity implements SensorE
                 flag=false;
                 timer.cancel();
                 timer2.cancel();
-                dialogTimer.cancel(); //in case the timer is started and user finished activity before the 3 hours are up
-                //fragebatterie
+                dialogTimer.cancel();
 
 
 
@@ -227,14 +222,13 @@ public class ActivitiesPageActivity extends AppCompatActivity implements SensorE
                 break;
         }
     }
-    
     void exitActivity() {
         sensorManager.unregisterListener(this, Accelerometer);
         startButton.setEnabled(true);
         stopButton.setEnabled(false);
         flag=false;
     }
-    
+
     void showAlertDialog() {
 
         AlertDialog.Builder builder = new AlertDialog.Builder(this);
@@ -261,17 +255,18 @@ public class ActivitiesPageActivity extends AppCompatActivity implements SensorE
         alertDialog.show();
 
     }
-    
+
     // 10800000ms = 3 hours
 
-   final CountDownTimer dialogTimer = new CountDownTimer(10800000, 1000) {
-          public void onTick(long millisUntilFinished) {
-          }
-                    public void onFinish () {
-                        if (flag == true) {
-                            showAlertDialog();
-                        }
-                    }
+    final CountDownTimer dialogTimer = new CountDownTimer(10800000, 1000) {
+        public void onTick(long millisUntilFinished) {
+        }
+        public void onFinish () {
+            if (flag == true) {
+                showAlertDialog();
+            }
+        }
 
-          };
+    };
+
 }
