28481a6187c3242dad45fd935c9e2d9a89d4a111
Ahmad Al Shihabi <ahmadalshihabi99@gmail.com>
2022-06-12 21:35:55 +0200

    Merge fixes

--- app/src/main/java/com/example/myfitnesstracker/view/activities/MainActivity.java
+++ app/src/main/java/com/example/myfitnesstracker/view/activities/MainActivity.java
@@ -1,7 +1,9 @@
 package com.example.myfitnesstracker.view.activities;
 
+import android.content.Context;
 import android.content.DialogInterface;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.SeekBar;
