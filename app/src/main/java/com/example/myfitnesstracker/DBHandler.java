fc5deab48b776b0f0e1bb12101d9787707ccbca7
derBirk <birk.wildhirt@ruhr-uni-bochum.de>
2022-06-24 18:27:15 +0200

    implemented better Mood table and method to retrieve mood data

--- app/src/main/java/com/example/myfitnesstracker/DBHandler.java
+++ app/src/main/java/com/example/myfitnesstracker/DBHandler.java
@@ -30,15 +30,23 @@ public class DBHandler extends SQLiteOpenHelper {
     // Start_Time defined above
 
     //define names for table Mood_Log
-    private final String TABLE_M_LOG = "Mood_Log";
-    private final String KEY_M_TIME = "Time";
-    private final String KEY_MOOD = "Mood";
+    private final String TABLE_MOOD_LOG = "Mood_Log";
+    private final String KEY_MOOD_TIME = "Time";
+    private final String KEY_MOOD_ZUFRIEDENHEIT = "Zufriedenheit";
+    private final String KEY_MOOD_RUHE = "Ruhe";
+    private final String KEY_MOOD_WOHL = "Wohlgefuehl";
+    private final String KEY_MOOD_SPANNUNG = "Entspannung";
+    private final String KEY_MOOD_ENERGIE = "Energie";
+    private final String KEY_MOOD_WACH = "Wachheit";
 
     //define names for table Questionnaires
     private final String TABLE_Quest = "Questionnaires";
     private final String KEY_version = "Version";
 
 
+    /**
+     * @param context the context from where the class is called, please use 'this'
+     */
     DBHandler(Context context){
         super(context.getApplicationContext(), DB_NAME, null, DB_V);
     }
@@ -53,7 +61,6 @@ public class DBHandler extends SQLiteOpenHelper {
                 + KEY_SENSOR_ZDATA + " REAL, "
                 + KEY_SENSOR_SOURCE + " TEXT" + ")";
         db.execSQL((CREATE_TABLE));
-
         //create table activity log
         CREATE_TABLE = "CREATE TABLE " + TABLE_ACTIVITY_LOG + "("
                 + KEY_ACTIVITY_AID + " INTEGER PRIMARY KEY AUTOINCREMENT, "
@@ -61,9 +68,16 @@ public class DBHandler extends SQLiteOpenHelper {
                 + KEY_ACTIVITY_S_TIME + " REAL, "
                 + KEY_ACTIVITY_E_TIME + " REAL" + ")";
         db.execSQL(CREATE_TABLE);
-        CREATE_TABLE = "CREATE TABLE " + TABLE_M_LOG + "()"
-                + KEY_M_TIME + " REAL PRIMARY KEY, "
-                + KEY_MOOD + " INT" + ")";
+        //create table mood log
+        CREATE_TABLE = "CREATE TABLE " + TABLE_MOOD_LOG + "("
+                + KEY_MOOD_TIME + " REAL PRIMARY KEY, "
+                + KEY_MOOD_ZUFRIEDENHEIT + " INT, "
+                + KEY_MOOD_RUHE + " INT, "
+                + KEY_MOOD_WOHL + " INT, "
+                + KEY_MOOD_SPANNUNG + " INT, "
+                + KEY_MOOD_ENERGIE + " INT, "
+                + KEY_MOOD_WACH + " INT" + ")";
+        db.execSQL(CREATE_TABLE);
     }
     @Override
     public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion){
@@ -147,7 +161,7 @@ public class DBHandler extends SQLiteOpenHelper {
     /**
      * returns an ArrayList with the data from the Activities Table
      * @param minTime minimum Time to consider as starting time
-     * @return ArrayList with ContentValues "Activity_ID", "Activity_Type", "Start_Time", "End_Time"
+     * @return ArrayList with ContentValues "Activity_ID", "Activity_Type", "Start_Time", "End_Time" or null if resulting table is empty
      */
     public ArrayList<ContentValues> getActivities(int minTime) {
         SQLiteDatabase db = this.getReadableDatabase();
@@ -187,7 +201,7 @@ public class DBHandler extends SQLiteOpenHelper {
     /**
      *
      * @param minTime minimum Time to consider
-     * @return ArrayList<ContentValues> with "time" and "value" values or null is table is empty
+     * @return ArrayList<ContentValues> with "time" and "value" vector length, or null if resulting table is empty
      */
     public ArrayList<ContentValues> getSensorData(float minTime){
         SQLiteDatabase db = this.getReadableDatabase();
@@ -200,6 +214,29 @@ public class DBHandler extends SQLiteOpenHelper {
             cv.put("time", cursor.getFloat(0));
             cv.put("value",vector.length());
             out.add(cv);
+            cursor.moveToNext();
+        }
+        cursor.close();
+        return out;
+    }
+
+    /**
+     * @return returns an arraylist of int[] with int[0] being the time and the rest being mood values in order of sampling scheme
+     */
+    public ArrayList<int[]> getMoodData(){
+        SQLiteDatabase db = this.getReadableDatabase();
+        Cursor cursor = db.query(TABLE_MOOD_LOG,null,null,null,null,null,null);
+        if (!cursor.moveToFirst()) return null;
+        ArrayList<int[]> out = new ArrayList<>(cursor.getCount());
+        while (!cursor.isAfterLast()) {
+            out.add(new int[]{
+                    cursor.getInt(0),
+                    cursor.getInt(1),
+                    cursor.getInt(2),
+                    cursor.getInt(3),
+                    cursor.getInt(4),
+                    cursor.getInt(5),
+                    cursor.getInt(6)});
             cursor.moveToNext();
         }
         cursor.close();
