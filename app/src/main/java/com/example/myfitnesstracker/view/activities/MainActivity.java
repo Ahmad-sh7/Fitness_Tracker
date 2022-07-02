bf74ef682e286f76e793583e1d239c2cfcd12828
bahaaabiad <bahaaabiad123@gmail.com>
2022-07-02 13:00:54 +0200

    Terms and Conditions and Privacy Policy question added

--- app/src/main/java/com/example/myfitnesstracker/view/activities/MainActivity.java
+++ app/src/main/java/com/example/myfitnesstracker/view/activities/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.myfitnesstracker.view.activities;
 
 import android.Manifest;
 import android.annotation.SuppressLint;
+import android.app.Dialog;
 import android.content.Context;
 import android.content.DialogInterface;
 import android.content.Intent;
@@ -9,12 +10,19 @@ import android.content.SharedPreferences;
 import android.content.pm.PackageManager;
 import android.location.Location;
 import android.os.Bundle;
+import android.text.Html;
+import android.text.method.LinkMovementMethod;
 import android.view.View;
 import android.widget.SeekBar;
+import android.widget.TextView;
 import android.widget.Toast;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AlertDialog;
+import androidx.cardview.widget.CardView;
 import androidx.core.app.ActivityCompat;
+import androidx.fragment.app.DialogFragment;
 
 import com.akexorcist.localizationactivity.ui.LocalizationActivity;
 import com.example.myfitnesstracker.R;
@@ -28,24 +36,28 @@ import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
 public class MainActivity extends LocalizationActivity {
 
+    public static final String PLANT_PLACES_PREFS = "PLANT_PLACES_PREFS";
+    public static final String TERMS_AND_CONDITIONS = "TERMS_AND_CONDITIONS";
     private FusedLocationProviderClient fusedLocationProviderClient;
     private LocationRequest locationRequest;
     private LocationCallback locationCallback;
+    private Bundle savedInstanceState;
 
     @SuppressLint("MissingPermission")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        this.savedInstanceState = savedInstanceState;
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-      // connect floating button to the settings page
+        // connect floating button to the settings page
         FloatingActionButton mySettings = (FloatingActionButton) findViewById(R.id.my_settings);
-         mySettings.setOnClickListener(new View.OnClickListener() {
+        mySettings.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 startActivity(new Intent(MainActivity.this, MySettings.class));
             }
         });
-        
+
         SharedPreferences settings = getSharedPreferences("mysettings", Context.MODE_PRIVATE);
         if (settings.getString("lang", "de").equals("en")) {
             setLanguage("en");
@@ -57,71 +69,63 @@ public class MainActivity extends LocalizationActivity {
         //access last location after permission
         fusedLocationProviderClient = LocationServices.getFusedLocationProviderClient(this);
 
-            fusedLocationProviderClient.getLastLocation().addOnSuccessListener(this, new OnSuccessListener<Location>() {
-                @Override
-                public void onSuccess(Location location) {
-                        if (location!=null){
-                            Toast.makeText(MainActivity.this, location.getLatitude()+" "+location.getLongitude(), Toast.LENGTH_SHORT).show();
-                        }
-                }
-            });
-
-          /** Called when the user taps the Log Activity card */
-
-        CardView cardView=findViewById(R.id.activityCard);
-        cardView.setOnClickListener(new View.OnClickListener()
-        {
+        fusedLocationProviderClient.getLastLocation().addOnSuccessListener(this, new OnSuccessListener<Location>() {
             @Override
-            public void onClick(View v)
-            {
-                Intent intent=new Intent(getApplicationContext(), ActivitiesPageActivity.class);
+            public void onSuccess(Location location) {
+                if (location != null) {
+                    Toast.makeText(MainActivity.this, location.getLatitude() + " " + location.getLongitude(), Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+
+        /** Called when the user taps the Log Activity card */
+
+        CardView cardView = findViewById(R.id.activityCard);
+        cardView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(), ActivitiesPageActivity.class);
                 startActivity(intent);
             }
         });
 
         /** Called when the user taps the Log Mood card */
-        CardView cardView2 =findViewById(R.id.moodCard);
-        cardView2.setOnClickListener(new View.OnClickListener()
-        {
+        CardView cardView2 = findViewById(R.id.moodCard);
+        cardView2.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View v)
-            {
-                Intent intent=new Intent(getApplicationContext(), MainActivity0.class);
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(), MainActivity0.class);
                 startActivity(intent);
             }
         });
 
         /** Called when the user taps the My Statistics card */
-        CardView cardView3 =findViewById(R.id.statisticsCard);
-        cardView3.setOnClickListener(new View.OnClickListener()
-        {
+        CardView cardView3 = findViewById(R.id.statisticsCard);
+        cardView3.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View v)
-            {
-                Intent intent=new Intent(getApplicationContext(), StatisticsPageActivity.class);
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(), StatisticsPageActivity.class);
                 startActivity(intent);
             }
         });
     }
 
-
     //ask for permission
-    private boolean isLocationPermissionGranted(){
-        boolean result=false;
-        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION)!=
+    private boolean isLocationPermissionGranted() {
+        boolean result = false;
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) !=
                 PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this,
-                Manifest.permission.ACCESS_FINE_LOCATION)!=PackageManager.PERMISSION_GRANTED){
+                Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
             ActivityCompat.requestPermissions(
                     this,
-                    new String[]{android.Manifest.permission.ACCESS_FINE_LOCATION,Manifest.permission.ACCESS_COARSE_LOCATION},55
+                    new String[]{android.Manifest.permission.ACCESS_FINE_LOCATION, Manifest.permission.ACCESS_COARSE_LOCATION}, 55
             );
-            result=true;
-        }
-        else{
-            result=false;
+            result = true;
+        } else {
+            result = false;
         }
 
-        return  result;
+        return result;
     }
 
     //Tagesabfrage
@@ -135,12 +139,11 @@ public class MainActivity extends LocalizationActivity {
         seek.setKeyProgressIncrement(1);
         builder.setView(seek);
         seek.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
-            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser){
+            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
                 int seekBarValue = seek.getProgress();
                 if (seekBarValue == 1) {
                     //fragbatterie um 20 uhr zeigen
-                }
-                else {
+                } else {
                     //wähle zeitraum wann fragebatterie gezeigt werden soll
                 }
             }
@@ -170,4 +173,35 @@ public class MainActivity extends LocalizationActivity {
 
     }
 
+    public class TermsAndConditionsDialogFragment extends DialogFragment {
+        @NonNull
+        @Override
+        public Dialog onCreateDialog(@Nullable Bundle savedInstanceState) {
+            AlertDialog.Builder builder = new AlertDialog.Builder(MainActivity.this);
+            builder.setMessage(Html.fromHtml("By using this app, you agree to our <a href=\"https://www.app-privacy-policy.com/live.php?token=X5oaGp9KJ0Idt4w5IfrPNQfHceIdBTOH\">Terms and Conditions</a> and <a href=\"https://www.app-privacy-policy.com/live.php?token=Ky6BZHHcnBWJi6MYFmCSIbPbnyXYxMqC\">Privacy Policy</a>"))
+                    .setPositiveButton(R.string.agree, new DialogInterface.OnClickListener() {
+                        @Override
+                        public void onClick(DialogInterface dialog, int which) {
+                            //make note that the user agree
+                            SharedPreferences prefs = MainActivity.this.getSharedPreferences(PLANT_PLACES_PREFS, Context.MODE_PRIVATE);
+                            SharedPreferences.Editor edit = prefs.edit();
+                            edit.putBoolean(TERMS_AND_CONDITIONS, true);
+                            edit.commit();
+                        }
+                    });
+            return builder.create();
+        }
+
+    }
+
+    protected void onResume() {
+        super.onResume();
+        SharedPreferences sharedPreferences= getSharedPreferences(PLANT_PLACES_PREFS, Context.MODE_PRIVATE);
+        if (!sharedPreferences.getBoolean(TERMS_AND_CONDITIONS, false)) {
+        MainActivity.TermsAndConditionsDialogFragment tsandcs = new MainActivity.TermsAndConditionsDialogFragment();
+        Dialog dialog = tsandcs.onCreateDialog(savedInstanceState);
+        dialog.show();
+        ((TextView) dialog.findViewById(android.R.id.message)).setMovementMethod(LinkMovementMethod.getInstance());
+        }
+    }
 }
